e74740855d7a0159c17f16fe8f56fd3604cf5cbf
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2024-06-08 20:38:01 +0530

    updated

--- src/components/ImageGenerator/Image.js
+++ src/components/ImageGenerator/Image.js
@@ -4,46 +4,46 @@ import { useRef, useState } from "react";
 import Default_image from "../assets/image.avif";
 
 const Image = () => {
-  const [imageUrl, setImageUrl] = useState(null);
+  const [imageUrl, setImageUrl] = useState("/");
   let inputRef = useRef(null);
   const [loading, setLoading] = useState(false);
-  const handleImageData = (data) => {
-    if (Array.isArray(data) && data.length > 0 && data[0].url) {
-      setImageUrl(data[0].url);
-    } else {
-      console.error("Invalid or empty data received from the API");
-      setImageUrl(Default_image);
-    }
-  };
 
   const ImageGenerator = async () => {
-    if (!inputRef.current.value) {
-      return;
-    }
-    setLoading(true);
-    try {
+    const token = "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";
+    try{
+      if (inputRef.current.value === "") {
+       return 0;
+      } 
+      setLoading(true);
       const response = await fetch(
-        "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
-        {
+       "https://api-inference.huggingface.co/models/Melonie/text_to_image_finetuned",
+       {
           method: "POST",
           headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer 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",
+           "Content-Type": "application/json",
+            Authorization: Bearer `${token}`,
             "User-Agent": "Chrome",
           },
           body: JSON.stringify({
-            prompt: inputRef.current.value,
+            prompt: `${inputRef.current.value}`,
             n: 1,
-            size: "512x512",
+            size: "512x512"
           }),
         }
       );
-      const data = await response.json();
-      handleImageData(data);
-    } catch (error) {
+      let data = await response.json();
+      console.log(data);
+      if (data && data.data && data.data.length > 0) {
+        let data_array = data.data;
+        setImageUrl(data_array[0].url);
+      } 
+      else {
+        console.error("data.data is undefined or empty");
+      }
+      setLoading(false);
+    } 
+    catch (error) {
       console.error("Error fetching data:", error);
-      setImageUrl(Default_image);
-    } finally {
       setLoading(false);
     }
   };
@@ -55,7 +55,7 @@ const Image = () => {
       </div>
       <div className="img-loading">
         <div className="image">
-          <img src={imageUrl || Default_image} alt=""></img>
+          <img src={imageUrl==="/" ? Default_image : imageUrl} alt=""></img>
         </div>
       </div>
       <div className="loading">
@@ -73,7 +73,7 @@ const Image = () => {
         />
         <div
           className="btn"
-          onClick={ImageGenerator}
+          onClick={() => {ImageGenerator()}}
         >
           Generate
         </div>
